d4f1aa0ef7239f0e9978f88fdfb49ae276ef1b8c
gnanagowsalyaai01 <kkowsalyaai01@gmail.com>
2024-02-01 13:30:17 +0530

    Modified oops example file

--- C++/oopsexample/main.cpp
+++ C++/oopsexample/main.cpp
@@ -2,6 +2,64 @@
 
 using namespace std;
 
+int main()
+{
+
+    string name;
+    cout<< "Enter your name : " << endl;
+    cin >> name;
+    cout << name << endl;
+    fflush(stdin);
+    cout<< "Enter your name : " << endl;
+    getline(cin,name);
+    cout << name << endl;
+
+
+  string firstname;
+  string lastname;
+  cin>> firstname >>lastname;
+
+  cout << firstname +" "+ lastname << endl ;
+  string fullname = firstname.append(lastname);
+  cout << fullname << endl;
+
+  lastname.push_back('k');
+  cout << lastname << endl;
+
+  cout << firstname << endl;
+   firstname.push_back('pp');
+  cout << firstname<< endl;
+
+  cout << firstname +" "+ lastname << endl ;
+
+
+
+  fflush(stdin);
+  //string fullname;
+  cin>> fullname;
+  fullname.insert(4," ag ");
+  cout << fullname;
+
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+/*#include <iostream>
+
+using namespace std;
+
 namespace employee
 {
     string name="Jo";
@@ -21,3 +79,4 @@ int main()
     cout << employee1::name << endl;
     return 0;
 }
+*/
