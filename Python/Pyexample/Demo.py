bfb092ed428f0f339ec46331c8f17d384f8c61e8
gnanagowsalyaai01 <kkowsalyaai01@gmail.com>
2024-02-07 13:15:33 +0530

    Added demoprogram files

---
+++ Python/Pyexample/Demo.py
@@ -0,0 +1,28 @@
+'''def subtraction():
+    a=4-1-1
+    print(a)
+    if a==3:
+      print("A is 3")
+      x="Kowsi is good girl"
+      for x1 in x:
+         print(x1)
+      x=x.split(" ")
+      for x2 in x:
+         print(x2)
+    elif a < 6:
+      print("A is Lessthan ")
+      z="Kowsi is naughty girl"
+      for x1 in z:
+         print(x1)
+      z=z.split(" ")
+      for x2 in z:
+         print(x2)
+    y=input("Enter the value:").split()
+    print(y)
+subtraction()'''
+
+
+k=0
+while k < 5:
+    k = k+1
+    print(k)
