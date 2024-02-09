e1e4c238520ddf41d034f93d0f19a24260d29e1b
gnanagowsalyaai01 <kkowsalyaai01@gmail.com>
2024-02-09 12:23:57 +0530

    Added familytree program file in python

---
+++ Python/Pyexample/famtree.py
@@ -0,0 +1,43 @@
+class grandparents:
+    def __init__(self,grandpaname,grandmaname,famname):
+        self.grandfathername=grandpaname
+        self.grandmothername=grandmaname
+        self.familyname=famname
+        
+
+    def welcome(self):
+        print(self.grandfathername,self.grandmothername, self.familyname)
+
+x=grandparents("Karunanidhi","Dayalu Ammal","DMK")
+x.welcome()
+
+class grandparents:
+    def __init__(self,grandpaname,grandmaname,famname):
+      self.grandfathername=grandpaname
+      self.grandmothername=grandmaname
+      self.familyname=famname
+        
+
+    def welcome(self):
+        print(" Welcome to " , self.familyname , " Wishes from " ,self.grandfathername, " and " ,self.grandmothername)
+
+
+class alagiri(grandparents):
+    def __init__(self, grandpaname, grandmaname, famname,paname,maname):
+          self.fathername=paname
+          self.mothername=maname
+          super().__init__(grandpaname, grandmaname, famname)
+  
+
+    def thank(self):
+        print("Hai!....Grandpa" ,self.grandfathername ,"and Grandma", self.grandmothername ,"We" , self.fathername, "and" ,self.mothername,  " thank you for warm welcome to our " , self.familyname, "family")
+
+x=alagiri("Karunanidhi","Dayalu ammal", "DMK","Alagiri", "Kanthi Alagiri")
+x.welcome()
+x.thank()
+
+
+
+
+
+        
